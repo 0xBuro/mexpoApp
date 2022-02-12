f7a9e51323183cfe9fd684f631755b5b42435d9d
Oguzhan-Burak Bozkurt <burakbozkurt94@gmail.com>
2022-02-12 17:24:05 +0100

    added Bitpanda URL to Constants

--- app/src/main/java/com/burakbo/mexpoapp/common/Constants.kt
+++ app/src/main/java/com/burakbo/mexpoapp/common/Constants.kt
@@ -2,4 +2,5 @@ package com.burakbo.mexpoapp.common
 
 object Constants {
     const val CRYPTO_ICON_URL = "https://cryptoicon-api.vercel.app/api/icon/"
+    const val BITPANDA_BASE_URL = "https://api.bitpanda.com/v1"
 }
--- app/src/main/java/com/burakbo/mexpoapp/data/remote/routes/HttpRoutes.kt
+++ app/src/main/java/com/burakbo/mexpoapp/data/remote/routes/HttpRoutes.kt
@@ -1,20 +1,21 @@
 package com.burakbo.mexpoapp.data.remote.routes
 
 import com.burakbo.mexpoapp.BuildConfig
+import com.burakbo.mexpoapp.common.Constants.BITPANDA_BASE_URL
 
 class HttpRoutes {
 
     object BitpandaHttpRoutes {
 
-        private const val BASE_URL = "https://api.bitpanda.com/v1"
+        private const val BASE_URL = BITPANDA_BASE_URL
         const val wallets = "$BASE_URL/wallets"
         const val bitpandaApiKey = BuildConfig.bitpandaApiKey
     }
 
     object KrakenHttpRoutes {
-        private const val BASE_URL = "https://api.bitpanda.com/v1"
-        const val wallets = "$BASE_URL/wallets"
-        const val bitpandaApiKey = BuildConfig.bitpandaApiKey
+        private const val BASE_URL = "KRAKEN_BASE_URL"
+        const val wallets = "$BASE_URL/KRAKEN_WALLET"
+        const val krakenApiKey = "KRAKEN_API_KEY"
     }
 
 }
